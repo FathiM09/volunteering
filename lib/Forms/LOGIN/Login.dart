6cfabf8d43cee9e868903571493bb63f8cc31772
fathim09 <fthimahmoud4@gmail.com>
2022-10-31 18:32:56 +0200

    Create Login file

---
+++ lib/Forms/LOGIN/Login.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class LOGING extends StatelessWidget {
+  const LOGING({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:volunteering/main.dart';
+import 'package:volunteering/Forms/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
